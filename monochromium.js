927dcd30ea96b3ffae2a4baaa52fa92f1c8d2aa6
Isaiah Odhner <isaiahodhner@gmail.com>
2013-06-03 13:28:35 -0700

    added browser back, forwards, reload buttons

--- monochromium.js
+++ monochromium.js
@@ -4,8 +4,30 @@ window.MonoChromium = function(url){
 	m.className = "webbrowser monochromium";
 	m.$m.className += " webbrowser monochromium";
 	m.title("Google MonoChrome");
-	m.content("<iframe/>");
+	m.content("<div>"
+		+"<button class='back'>&#x2190;</button>"
+		+"<button class='forwards'>&#x2192;</button>"
+		+"<button class='reload'>&#x21BB;</button>"
+		+"<input/><button class='hotdog'>&equiv;</button>"
+		+"</div><iframe>");
 	var $if = m.$("iframe");
-	$if.src = ("http://"+(url||"1j01.koding.com")).replace();
+	var $in = m.$("input"); $in.value=url||"1j01.koding.com";
+	$in.style.width="calc(100% - 112px)";
+	$if.src = "http://"+$in.value.replace(/\w+:\/?\/?/,"");
+	m.$(".back").onclick=function(){
+		$if.contentWindow.history.go(-1);
+	};
+	m.$(".forwards").onclick=function(){
+		$if.contentWindow.history.go(1);
+	};
+	m.$(".reload").onclick=function(){
+		$if.contentWindow.history.go(0);
+	};
+	$in.onkeypress=function(e){
+		if(e.keyCode==13){
+			$if.src = "http://"+$in.value.replace(/\w+:\/?\/?/,"");
+			$in.value="";
+		}
+	};
 	return m;
 };
